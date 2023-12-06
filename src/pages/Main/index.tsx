12d831e14e23ab468bb072329fcdbff9d7694bdf
laireyx <laireyx@gmail.com>
2023-12-06 23:52:21 +0900

    feat: set initial data to main page query

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -13,6 +13,44 @@ export default function Main() {
   const { data } = useSuspenseQuery({
     queryFn: queryGithubProfile,
     queryKey: ['gh-api', 'profile'],
+    initialData: {
+      login: 'laireyx',
+      id: 82489493,
+      node_id: 'MDQ6VXNlcjgyNDg5NDkz',
+      avatar_url: 'https://avatars.githubusercontent.com/u/82489493?v=4',
+      gravatar_id: '',
+      url: 'https://api.github.com/users/laireyx',
+      html_url: 'https://github.com/laireyx',
+      followers_url: 'https://api.github.com/users/laireyx/followers',
+      following_url:
+        'https://api.github.com/users/laireyx/following{/other_user}',
+      gists_url: 'https://api.github.com/users/laireyx/gists{/gist_id}',
+      starred_url:
+        'https://api.github.com/users/laireyx/starred{/owner}{/repo}',
+      subscriptions_url: 'https://api.github.com/users/laireyx/subscriptions',
+      organizations_url: 'https://api.github.com/users/laireyx/orgs',
+      repos_url: 'https://api.github.com/users/laireyx/repos',
+      events_url: 'https://api.github.com/users/laireyx/events{/privacy}',
+      received_events_url:
+        'https://api.github.com/users/laireyx/received_events',
+      type: 'User',
+      site_admin: false,
+      name: 'Minsoo Park',
+      company: null,
+      blog: 'https://laireyx.net',
+      location: 'Seoul, Republic of Korea',
+      email: null,
+      hireable: null,
+      bio: null,
+      twitter_username: null,
+      public_repos: 21,
+      public_gists: 0,
+      followers: 4,
+      following: 5,
+      created_at: '2021-04-14T03:54:20Z',
+      updated_at: '2023-12-06T14:14:26Z',
+    },
+    initialDataUpdatedAt: 0,
   });
 
   return (
