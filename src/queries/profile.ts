fd3cc4e0b1829598252ae1c41c505b80a081d852
laireyx <laireyx@gmail.com>
2023-12-06 22:56:29 +0900

    fix: correct VITE_API_URL setup api url env.var while building

--- src/queries/profile.ts
+++ src/queries/profile.ts
@@ -1,6 +1,6 @@
 import { Endpoints } from '@octokit/types';
 
 export default async function queryGithubProfile() {
-  const resp = await fetch(new URL('/profile', import.meta.env.VITE_API_URL));
+  const resp = await fetch(`${import.meta.env.VITE_API_URL}/profile`);
   return (await resp.json()) as Endpoints['GET /users/{username}']['response']['data'];
 }
--- src/queries/repos.ts
+++ src/queries/repos.ts
@@ -1,8 +1,6 @@
 import { Endpoints } from '@octokit/types';
 
 export default async function queryGithubRepo(repo: string) {
-  const resp = await fetch(
-    new URL(`/repos/${repo}`, import.meta.env.VITE_API_URL),
-  );
+  const resp = await fetch(`${import.meta.env.VITE_API_URL}/repos/${repo}`);
   return (await resp.json()) as Endpoints['GET /repos/{owner}/{repo}']['response']['data'];
 }
