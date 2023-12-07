4c5f10a6ea3f480ffdbaecb62d2ba7cbbf2c691c
laireyx <laireyx@gmail.com>
2023-12-07 09:55:45 +0900

    lint: ignore false positive react-refresh warnings

--- src/router.tsx
+++ src/router.tsx
@@ -10,8 +10,10 @@ import Main from '@pages/Main';
 
 import App from './App';
 
+/* eslint-disable react-refresh/only-export-components */
 const Projects = lazy(() => import('@pages/Projects'));
 const Music = lazy(() => import('@pages/Music'));
+/* eslint-enable react-refresh/only-export-components */
 
 export const router = createHashRouter(
   createRoutesFromElements(
